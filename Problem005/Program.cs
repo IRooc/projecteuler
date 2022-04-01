4624ad9073323334cb81784221d986eb5bab0a28
Roderic Bos <github@rooc.nl>
2022-04-01 20:24:54 +0200

    Problem 5

---
+++ Problem005/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+*/
+
+Int64 max = 1;
+for (int i = 1; i <= 20; i++)
+{
+   max *= i;
+}
+Console.WriteLine("Max number " + max);
+Int64 cur = 20;
+while (cur < max)
+{
+   bool success = true;
+   for (int i = 1; i <= 20; i++)
+   {
+      if (cur % i != 0)
+      {
+         success = false;
+         break;
+      }
+   }
+   if (success){
+      Console.WriteLine(cur);
+      break;
+   }
+
+   cur++;
+}
