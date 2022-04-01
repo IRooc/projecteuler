becbf336400fb17d49a299429e4bf234bb879b2e
Roderic Bos <github@rooc.nl>
2022-04-01 21:05:40 +0200

    Problem 6

---
+++ Problem006/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+The sum of the squares of the first ten natural numbers is,
+ 1^2  + 2^2 + ..... + 10^2 = 385
+The square of the sum of the first ten natural numbers is,
+   (1 + 2 + 3 + ... + 10)^2 = 55^2 = 3025
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is .
+3025 - 385 = 2640
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+*/
+
+Int64 squareSum = 0;
+Int64 sum = 0;
+for (int i = 1; i <= 100; i++){
+   squareSum += (i*i);
+   sum += i;
+}
+Int64 sumSquare = sum*sum;
+Console.WriteLine(sumSquare - squareSum);
