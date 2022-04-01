f36820f4096d9d15b0c933559fa9f14fd58a0c5d
Roderic Bos <github@rooc.nl>
2022-04-01 20:19:04 +0200

    Problem 3

---
+++ Problem003/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+
+*/
+Int64 i = 1;
+var primeFactors = new List<Int64>();
+while (true) {
+   i++;
+   bool isprime = true;
+   for (int j = 2; j <= i / 2; j++)
+   {
+      if (i % j == 0)
+      {
+         isprime = false;
+         break;
+      }
+   }
+   
+   if (isprime && (600851475143 % i == 0)) {
+      primeFactors.Add(i);
+   }
+   Int64 factor = 1;
+   foreach(var p in primeFactors){
+      factor *= p;
+   }
+   if (factor == 600851475143) break;
+}
+Console.WriteLine(string.Join(",",primeFactors.Select(p=>p.ToString())));
