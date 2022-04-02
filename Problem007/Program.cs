9a8ad968534dbd438a8fab5d7dc785fd22e358ef
Roderic Bos <github@rooc.nl>
2022-04-02 17:55:05 +0200

    Problems 7 8 and 9

---
+++ Problem007/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+*/
+Int64 i = 1;
+var primeCount = 0;
+while (true) {
+   i++;
+   bool isprime = true;
+   for (int j = 2; j <= i / 2; j++)
+   {
+      if (i % j == 0)
+      {
+         isprime = false;
+         break;
+      }
+   }
+   
+   if (isprime) {
+      primeCount++;
+   }
+   if (primeCount == 10001) break;
+}
+Console.WriteLine(i);
---
+++ Problem009/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+a2 + b2 = c2
+For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+
+*/
+var found = false;
+for (int a = 1; a < 500; a++)
+{
+   for (int b = 1; b < 500; b++)
+   {
+      for (int c = 1; c < 500; c++)
+      {
+         if (a + b + c == 1000)
+         {
+            if (a * a + b * b == c * c)
+            {
+               Console.WriteLine($"Found Pythagorean {a} {b} {c}");
+               Console.WriteLine($"Found Product {a*b*c}");
+               found = true;
+               break;
+            }
+         }
+      }
+      if (found) break;
+   }
+   if (found) break;
+}
